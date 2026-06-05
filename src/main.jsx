a88a77a2cb22c250f2b883950711957b64d7059b
Cyril Abraham Onyekachukwu <abby@Cyrils-MacBook-Air.local>
2026-06-05 02:27:43 +0100

    unregister sw on load

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Unregister all service workers and clear caches
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(registrations => {
+    registrations.forEach(reg => reg.unregister())
+  })
+  caches.keys().then(keys => {
+    keys.forEach(key => caches.delete(key))
+  })
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
